2a5f8c9404783ffc567b036c88b5e5c3c5fad054
Raiza De Guzman <santos_dg7@hotmail.com>
2021-12-17 21:18:00 -0800

    Initial commit with all my javascripting work so far

---
+++ looping-through-arrays.js
@@ -0,0 +1,12 @@
+const pets = ["cat", "dog", "rat"];
+for (var i = 0; i < pets.length; i++) {
+  pets[i] = pets[i] + "s";
+}
+console.log(pets);
+
+// SOLUTION PROVIDED:
+// const pets = ["cat", "dog", "rat"];
+// for (let i = 0; i < pets.length; i++) {
+//   pets[i] = pets[i] + "s";
+// }
+// console.log(pets);
---
+++ number-to-string.js
@@ -0,0 +1,8 @@
+let n = 128;
+n = n.toString();
+console.log(n);
+
+// OR ANOTHER (SHORTER) WAY (according to the solution in the exericse):
+
+const n = 128;
+console.log(n.toString());
